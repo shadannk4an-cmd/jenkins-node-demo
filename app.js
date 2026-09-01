06028f57dcf2646c3e7b7fadb5b93fb9fe50891b
Shadan Hussain Khan <shadank4an@gmail.com>
2026-09-01 23:43:36 +0530

    test deployment rollback 2

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ if (require.main === module) {
         }
 
         // Normal application response
-        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
         res.end(greet('Jenkins CI/CD'));
     });
 
